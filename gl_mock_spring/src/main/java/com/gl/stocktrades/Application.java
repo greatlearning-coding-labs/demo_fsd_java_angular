623515f7c89a73654f3dc715de9801f4ba19f6c3
sumit <108910264+sumit-davda@users.noreply.github.com>
2026-03-05 12:39:18 +0530

    Update Application.java

--- gl_mock_spring/src/main/java/com/gl/stocktrades/Application.java
+++ gl_mock_spring/src/main/java/com/gl/stocktrades/Application.java
@@ -1,4 +1,4 @@
-package com.hackerrank.stocktrades;
+package com.gl.stocktrades;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
