4a50b7256c7b56844e58b8db607e11cb4d48b9d9
sumit <108910264+sumit-davda@users.noreply.github.com>
2026-03-05 12:39:45 +0530

    Update StockTypeValidator.java

--- gl_mock_spring/src/main/java/com/gl/stocktrades/validation/StockTypeValidator.java
+++ gl_mock_spring/src/main/java/com/gl/stocktrades/validation/StockTypeValidator.java
@@ -1,4 +1,4 @@
-package com.hackerrank.stocktrades.validation;
+package com.gl.stocktrades.validation;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
